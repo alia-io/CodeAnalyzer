b9135fa4aff3e52cb133cea4647b769005d93be9
Alifa Stith <alifastith@gmail.com>
2021-02-15 23:08:26 -0500

    Unit testing project started

---
+++ CodeAnalyzerTests/InputOutputTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeAnalyzer;
+using System;
+using System.IO;
+
+namespace CodeAnalyzerTests
+{
+    [TestClass]
+    public class InputReaderTests
+    {
+        [TestMethod]
+        public void TestInputReader_FormatInput()
+        {
+            string[] input = { "/X", ".", "*.cs", "/s" };
+            string[] expectedFormattedInput = { "/S", "", "/X", Path.GetFullPath("."), "*.cs" };
+            string[] actualFormattedInput;
+
+            InputReader inputReader = new InputReader();
+            inputReader.FormatInput(input);
+            actualFormattedInput = inputReader.FormattedInput;
+
+            CollectionAssert.AreEquivalent(expectedFormattedInput, actualFormattedInput);
+        }
+    }
+
+    [TestClass]
+    public class OutputWriterTests
+    {
+
+    }
+}
