6eab12ca0254cda65b9ac8a8a74b3c7a5e871e0e
Alifa Stith <alifastith@gmail.com>
2021-02-15 23:24:13 -0500

    InputReader tests

--- CodeAnalyzerTests/InputOutputTests.cs
+++ CodeAnalyzerTests/InputOutputTests.cs
@@ -9,10 +9,38 @@ namespace CodeAnalyzerTests
     public class InputReaderTests
     {
         [TestMethod]
-        public void TestInputReader_FormatInput()
+        public void TestInputReader_FormatInput_1()
         {
             string[] input = { "/X", ".", "*.cs", "/s" };
             string[] expectedFormattedInput = { "/S", "", "/X", Path.GetFullPath("."), "*.cs" };
+            string[] actualFormattedInput;
+
+            InputReader inputReader = new InputReader();
+            inputReader.FormatInput(input);
+            actualFormattedInput = inputReader.FormattedInput;
+
+            CollectionAssert.AreEquivalent(expectedFormattedInput, actualFormattedInput);
+        }
+
+        [TestMethod]
+        public void TestInputReader_FormatInput_2()
+        {
+            string[] input = { "/s", "/r", "/X", "*.txt", "." };
+            string[] expectedFormattedInput = { "/S", "/R", "/X", Path.GetFullPath("."), "*.txt" };
+            string[] actualFormattedInput;
+
+            InputReader inputReader = new InputReader();
+            inputReader.FormatInput(input);
+            actualFormattedInput = inputReader.FormattedInput;
+
+            CollectionAssert.AreEquivalent(expectedFormattedInput, actualFormattedInput);
+        }
+
+        [TestMethod]
+        public void TestInputReader_FormatInput_3()
+        {
+            string[] input = { "*.cs", "C:" };
+            string[] expectedFormattedInput = { "", "", "", Path.GetFullPath("C:"), "*.cs" };
             string[] actualFormattedInput;
 
             InputReader inputReader = new InputReader();
